1f77b4e35ea03ba4f04a3cc8a336e18d745fc93f
Sheena Nickerson <sheenanick@gmail.com>
2016-11-18 09:09:00 -0800

    bind views with ButterKnife

--- app/src/main/java/com/epicodus/pdxbranch/SignUpActivity.java
+++ app/src/main/java/com/epicodus/pdxbranch/SignUpActivity.java
@@ -2,12 +2,23 @@ package com.epicodus.pdxbranch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity {
+    @Bind(R.id.firstNameEditText) EditText mFirstNameEditText;
+    @Bind(R.id.lastNameEditText) EditText mLastNameEditText;
+    @Bind(R.id.screenNameEditText) EditText mScreenNameEditText;
+    @Bind(R.id.cityEditText) EditText mCityEditText;
+    @Bind(R.id.stateEditText) EditText mStateEditText;
+    @Bind(R.id.zipCodeEditText) EditText mZipCodeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
     }
 }
