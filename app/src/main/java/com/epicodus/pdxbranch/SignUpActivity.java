cd748a495fd80e2a1af42ffdade17b01d05b44b3
Sheena Nickerson <sheenanick@gmail.com>
2016-11-18 09:15:21 -0800

    attach click listener to submit button

--- app/src/main/java/com/epicodus/pdxbranch/SignUpActivity.java
+++ app/src/main/java/com/epicodus/pdxbranch/SignUpActivity.java
@@ -1,14 +1,16 @@
 package com.epicodus.pdxbranch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.firstNameEditText) EditText mFirstNameEditText;
     @Bind(R.id.lastNameEditText) EditText mLastNameEditText;
     @Bind(R.id.screenNameEditText) EditText mScreenNameEditText;
@@ -22,5 +24,14 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         ButterKnife.bind(this);
+
+        mSubmitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSubmitButton) {
+
+        }
     }
 }
