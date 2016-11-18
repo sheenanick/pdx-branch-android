8a08950106d5f735ee2411f18c46890c67233a00
Sheena Nickerson <sheenanick@gmail.com>
2016-11-18 10:32:41 -0800

    remove placeholder text and replace with greeting message and list view - application crashes after adding several posts

--- app/src/main/java/com/epicodus/pdxbranch/DashboardActivity.java
+++ app/src/main/java/com/epicodus/pdxbranch/DashboardActivity.java
@@ -3,19 +3,32 @@ package com.epicodus.pdxbranch;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class DashboardActivity extends AppCompatActivity {
-    @Bind(R.id.testTextView) TextView mTestTextView;
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.greetingTextView) TextView mGreetingTextView;
+    @Bind(R.id.addPostEditText) EditText mAddPostEditText;
+    @Bind(R.id.postButton) Button mPostButton;
+    @Bind(R.id.newsFeedListView) ListView mNewsFeedListView;
+    ArrayList<String> posts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         ButterKnife.bind(this);
+
+        mPostButton.setOnClickListener(this);
 
         Intent intent = getIntent();
         String first_name = intent.getStringExtra("first_name");
@@ -24,7 +37,18 @@ public class DashboardActivity extends AppCompatActivity {
         String city = intent.getStringExtra("city");
         String state = intent.getStringExtra("state");
         String zip_code = intent.getStringExtra("zip_code");
-        String text = "first_name: " + first_name + ", last_name: " + last_name + ", screen_name: " + screen_name + ", city: " + city + ", state: " + state + ", zip_code: " + zip_code;
-        mTestTextView.setText(text);
+
+        String greeting = "Hello, " + first_name;
+        mGreetingTextView.setText(greeting);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, posts);
+        mNewsFeedListView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mPostButton) {
+            posts.add(mAddPostEditText.getText().toString());
+        }
     }
 }
