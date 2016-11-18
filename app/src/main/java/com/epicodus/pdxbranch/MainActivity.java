9aed1e1fc6baa1b0e929ea058460278d57a44fcc
Sheena Nickerson <sheenanick@gmail.com>
2016-11-18 13:39:41 -0800

    change appbar to toolbar

--- app/src/main/java/com/epicodus/pdxbranch/MainActivity.java
+++ app/src/main/java/com/epicodus/pdxbranch/MainActivity.java
@@ -3,6 +3,7 @@ package com.epicodus.pdxbranch;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,12 +12,16 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.signUpButton) Button mSignUpButton;
+    @Bind(R.id.toolbar) Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         mSignUpButton.setOnClickListener(this);
     }
