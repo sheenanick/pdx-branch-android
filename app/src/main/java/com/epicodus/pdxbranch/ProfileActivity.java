6d4d7050e186ab1a09e9c86d26359bb838dd84e5
Sheena Nickerson <sheenanick@gmail.com>
2016-11-18 14:45:20 -0800

    add up button to ProfileActivity toolbar

--- app/src/main/java/com/epicodus/pdxbranch/ProfileActivity.java
+++ app/src/main/java/com/epicodus/pdxbranch/ProfileActivity.java
@@ -1,13 +1,25 @@
 package com.epicodus.pdxbranch;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ProfileActivity extends AppCompatActivity {
+    @Bind(R.id.toolbar) Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
