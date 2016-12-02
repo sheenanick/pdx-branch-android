6744c6fe62edfbca683d796760824a12f404a236
Sheena Nickerson <sheenanick@gmail.com>
2016-12-02 11:32:52 -0800

    add toolbar

--- app/src/main/java/com/epicodus/pdxbranch/ui/MeetupGroupDetailActivity.java
+++ app/src/main/java/com/epicodus/pdxbranch/ui/MeetupGroupDetailActivity.java
@@ -2,14 +2,22 @@ package com.epicodus.pdxbranch.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.epicodus.pdxbranch.R;
 
+import butterknife.Bind;
+
 public class MeetupGroupDetailActivity extends AppCompatActivity {
+    @Bind(R.id.toolbar) Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meetup_group);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
