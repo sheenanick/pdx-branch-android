3aa2d1c6e9a7023376a2d0aee720cf253b0f27d1
Sheena Nickerson <sheenanick@gmail.com>
2016-12-02 08:52:33 -0800

    add empty Meetup group detail activity and fragment

---
+++ app/src/main/java/com/epicodus/pdxbranch/ui/MeetupGroupDetailActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.pdxbranch.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.pdxbranch.R;
+
+public class MeetupGroupDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meetup_group);
+    }
+}
