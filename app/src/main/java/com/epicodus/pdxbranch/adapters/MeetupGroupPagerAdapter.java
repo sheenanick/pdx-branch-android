e942739e4dda93270718c0386123267452f48499
Sheena Nickerson <sheenanick@gmail.com>
2016-12-02 12:24:50 -0800

    add methods to pager adapter

--- app/src/main/java/com/epicodus/pdxbranch/adapters/MeetupGroupPagerAdapter.java
+++ app/src/main/java/com/epicodus/pdxbranch/adapters/MeetupGroupPagerAdapter.java
@@ -1,7 +1,29 @@
 package com.epicodus.pdxbranch.adapters;
 
-/**
- * Created by Guest on 12/2/16.
- */
-public class MeetupGroupPagerAdapter {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.epicodus.pdxbranch.models.MeetupGroup;
+import com.epicodus.pdxbranch.ui.MeetupGroupDetailFragment;
+
+import java.util.ArrayList;
+
+public class MeetupGroupPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<MeetupGroup> mMeetupGroups;
+
+    public MeetupGroupPagerAdapter(FragmentManager fm, ArrayList<MeetupGroup> meetupGroups) {
+        super(fm);
+        mMeetupGroups = meetupGroups;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return MeetupGroupDetailFragment.newInstance(mMeetupGroups.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mMeetupGroups.size();
+    }
 }
