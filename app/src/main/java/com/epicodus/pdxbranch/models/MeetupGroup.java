f3c708fedd07dc4de9b44430ebb53d75e9a8a5bf
Sheena Nickerson <sheenanick@gmail.com>
2016-12-01 18:57:38 -0800

    add MeetupGroup Model

---
+++ app/src/main/java/com/epicodus/pdxbranch/models/MeetupGroup.java
@@ -0,0 +1,63 @@
+package com.epicodus.pdxbranch.models;
+
+import java.util.ArrayList;
+
+public class MeetupGroup {
+    private String mName;
+    private String mMeetupLink;
+    private String mDescription;
+    private int mDateCreated;
+    private int mNumOfMembers;
+    private String mOrganizerName;
+    private String mOrganizerPhotoThumb;
+    private String mGroupPhotoThumb;
+    private ArrayList<String> mPhotos;
+
+    public MeetupGroup(String mName, String mMeetupLink, String mDescription, int mDateCreated, int mNumOfMembers, String mOrganizerName, String mOrganizerPhotoThumb, String mGroupPhotoThumb, ArrayList<String> mPhotos) {
+        this.mName = mName;
+        this.mMeetupLink = mMeetupLink;
+        this.mDescription = mDescription;
+        this.mDateCreated = mDateCreated;
+        this.mNumOfMembers = mNumOfMembers;
+        this.mOrganizerName = mOrganizerName;
+        this.mOrganizerPhotoThumb = mOrganizerPhotoThumb;
+        this.mGroupPhotoThumb = mGroupPhotoThumb;
+        this.mPhotos = mPhotos;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public String getmMeetupLink() {
+        return mMeetupLink;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public int getmDateCreated() {
+        return mDateCreated;
+    }
+
+    public int getmNumOfMembers() {
+        return mNumOfMembers;
+    }
+
+    public String getmOrganizerName() {
+        return mOrganizerName;
+    }
+
+    public String getmOrganizerPhotoThumb() {
+        return mOrganizerPhotoThumb;
+    }
+
+    public String getmGroupPhotoThumb() {
+        return mGroupPhotoThumb;
+    }
+
+    public ArrayList<String> getmPhotos() {
+        return mPhotos;
+    }
+}
