77618acad1dd5557860df70e10ce759ea44361b7
Sheena Nickerson <sheenanick@gmail.com>
2016-12-09 12:56:30 -0800

    add progress dialog

--- app/src/main/java/com/epicodus/pdxbranch/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/pdxbranch/ui/CreateAccountActivity.java
@@ -1,5 +1,6 @@
 package com.epicodus.pdxbranch.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -28,6 +29,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private ProgressDialog mAuthProgressDialog;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +39,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         mCreateUserButton.setOnClickListener(this);
         mAuth = FirebaseAuth.getInstance();
 
+        createAuthProgressDialog();
         createAuthStateListener();
     }
 
@@ -45,6 +48,36 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         if (v == mCreateUserButton) {
             createNewUser();
         }
+    }
+
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
+    }
+
+    private void createNewUser() {
+        final String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        String confirmPassword = mConfirmPassword.getText().toString().trim();
+
+        boolean validEmail = isValidEmail(email);
+        boolean validPassword = isValidPassword(password, confirmPassword);
+
+        if (!validEmail || !validPassword) return;
+        mAuthProgressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        mAuthProgressDialog.dismiss();
+                        if (!task.isSuccessful()){
+                            Toast.makeText(CreateAccountActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private boolean isValidEmail(String email) {
@@ -64,27 +97,6 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
             return false;
         }
         return true;
-    }
-
-    private void createNewUser() {
-        final String email = mEmail.getText().toString().trim();
-        String password = mPassword.getText().toString().trim();
-        String confirmPassword = mConfirmPassword.getText().toString().trim();
-
-        boolean validEmail = isValidEmail(email);
-        boolean validPassword = isValidPassword(password, confirmPassword);
-
-        if (!validEmail || !validPassword) return;
-
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (!task.isSuccessful()){
-                            Toast.makeText(CreateAccountActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
     }
 
     private void createAuthStateListener() {
