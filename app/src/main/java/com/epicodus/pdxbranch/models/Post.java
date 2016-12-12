d269fe269daca9948d3550dcddac27a6d9c7eb78
Sheena Nickerson <sheenanick@gmail.com>
2016-12-11 18:20:43 -0800

    add author attribute to Post class

--- app/src/main/java/com/epicodus/pdxbranch/models/Post.java
+++ app/src/main/java/com/epicodus/pdxbranch/models/Post.java
@@ -4,14 +4,20 @@ import org.parceler.Parcel;
 
 @Parcel
 public class Post {
+    private String author;
     private String content;
     private String pushId;
 
     public Post() {}
 
-    public Post(String content, String uid) {
+    public Post(String author, String content, String uid) {
+        this.author = author;
         this.content = content;
         this.pushId = uid;
+    }
+
+    public String getAuthor() {
+        return author;
     }
 
     public String getContent() {
