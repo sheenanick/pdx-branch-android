288b7bf2223714c8489890c9141316239f634cc7
Sheena Nickerson <sheenanick@gmail.com>
2016-12-11 18:33:50 -0800

    remove first name and last name keys for Shared Preferences

--- app/src/main/java/com/epicodus/pdxbranch/Constants.java
+++ app/src/main/java/com/epicodus/pdxbranch/Constants.java
@@ -4,6 +4,4 @@ public class Constants {
     public static final String MEETUP_API_KEY = BuildConfig.MEETUP_API_KEY;
     public static final String MEETUP_API_KEY_PARAMETER = "key";
     public static final String MEETUP_BASE_URL = "https://api.meetup.com/recommended/groups?photo-host=public&page=5&sign=true&category=34";
-    public static final String PREFERENCES_FIRST_NAME_KEY = "firstName";
-    public static final String PREFERENCES_LAST_NAME_KEY = "lastName";
 }
