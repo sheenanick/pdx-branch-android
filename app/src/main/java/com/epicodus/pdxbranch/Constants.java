9623cd77507fe2cc883adde4c46eefba3973056a
Sheena Nickerson <sheenanick@gmail.com>
2016-12-11 21:04:49 -0800

    add search parameter to constants

--- app/src/main/java/com/epicodus/pdxbranch/Constants.java
+++ app/src/main/java/com/epicodus/pdxbranch/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final String MEETUP_API_KEY = BuildConfig.MEETUP_API_KEY;
     public static final String MEETUP_API_KEY_PARAMETER = "key";
     public static final String MEETUP_BASE_URL = "https://api.meetup.com/find/groups?photo-host=public&page=10&sign=true&category=34";
+    public static final String PREFERENCES_SEARCH_KEY = "text";
 }
