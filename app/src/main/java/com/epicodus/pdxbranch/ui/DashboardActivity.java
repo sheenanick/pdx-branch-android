11af38cc84cc2cf8b2e8c1454a491048454f5fcc
Sheena Nickerson <sheenanick@gmail.com>
2016-12-14 18:04:05 -0800

    pass current user id with intent to start Profile Activity when clicking on profile icon

--- app/src/main/java/com/epicodus/pdxbranch/ui/DashboardActivity.java
+++ app/src/main/java/com/epicodus/pdxbranch/ui/DashboardActivity.java
@@ -141,6 +141,7 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
 
             case R.id.action_profile:
                 Intent profileIntent = new Intent(DashboardActivity.this, ProfileActivity.class);
+                profileIntent.putExtra("authorId", mCurrentUserId);
                 startActivity(profileIntent);
                 return true;
 
