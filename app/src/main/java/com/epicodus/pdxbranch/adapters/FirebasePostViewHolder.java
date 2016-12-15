9c26876315ae0a8b427fe5808b01247ca73fc8c3
Sheena Nickerson <sheenanick@gmail.com>
2016-12-14 18:00:40 -0800

    add click listener to author text view, so that Profile Activity will start on click

--- app/src/main/java/com/epicodus/pdxbranch/adapters/FirebasePostViewHolder.java
+++ app/src/main/java/com/epicodus/pdxbranch/adapters/FirebasePostViewHolder.java
@@ -1,6 +1,7 @@
 package com.epicodus.pdxbranch.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
@@ -8,11 +9,15 @@ import android.widget.TextView;
 
 import com.epicodus.pdxbranch.R;
 import com.epicodus.pdxbranch.models.Post;
+import com.epicodus.pdxbranch.ui.DashboardActivity;
+import com.epicodus.pdxbranch.ui.ProfileActivity;
 import com.squareup.picasso.Picasso;
 
-public class FirebasePostViewHolder extends RecyclerView.ViewHolder{
+public class FirebasePostViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     private static final int MAX_WIDTH = 100;
     private static final int MAX_HEIGHT = 100;
+    private TextView mUserNameTextView;
+    private String mPostAuthorId;
     View mView;
     Context mContext;
 
@@ -24,15 +29,27 @@ public class FirebasePostViewHolder extends RecyclerView.ViewHolder{
 
     public void bindPost(Post post) {
         ImageView memberProfileImageView = (ImageView) mView.findViewById(R.id.profileImageView);
-        TextView userNameTextView = (TextView) mView.findViewById(R.id.userNameTextView);
+        mUserNameTextView = (TextView) mView.findViewById(R.id.userNameTextView);
         TextView contentTextView = (TextView) mView.findViewById(R.id.contentTextView);
+        mPostAuthorId = post.getAuthorId();
 
-        userNameTextView.setText(post.getAuthor());
+        mUserNameTextView.setText(post.getAuthor());
         contentTextView.setText(post.getContent());
         Picasso.with(mContext)
                 .load(post.getAuthorImageUrl())
                 .resize(MAX_WIDTH, MAX_HEIGHT)
                 .centerCrop()
                 .into(memberProfileImageView);
+
+        mUserNameTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mUserNameTextView) {
+            Intent intent = new Intent(mContext, ProfileActivity.class);
+            intent.putExtra("authorId", mPostAuthorId);
+            mContext.startActivity(intent);
+        }
     }
 }
