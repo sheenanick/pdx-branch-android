1a14100d15c4c7bfdd27a7eae250ba260ba29eba
Sheena Nickerson <sheenanick@gmail.com>
2016-12-16 14:57:05 -0800

    decode bitmap image and set to image view

--- app/src/main/java/com/epicodus/pdxbranch/adapters/FirebasePostViewHolder.java
+++ app/src/main/java/com/epicodus/pdxbranch/adapters/FirebasePostViewHolder.java
@@ -2,7 +2,10 @@ package com.epicodus.pdxbranch.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +15,8 @@ import com.epicodus.pdxbranch.models.Post;
 import com.epicodus.pdxbranch.ui.DashboardActivity;
 import com.epicodus.pdxbranch.ui.ProfileActivity;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 public class FirebasePostViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     private static final int MAX_WIDTH = 100;
@@ -27,12 +32,20 @@ public class FirebasePostViewHolder extends RecyclerView.ViewHolder implements V
         mContext = itemView.getContext();
     }
 
-    public void bindPost(Post post) {
+    public void bindPost(Post post) throws IOException {
         ImageView memberProfileImageView = (ImageView) mView.findViewById(R.id.profileImageView);
         mUserNameTextView = (TextView) mView.findViewById(R.id.userNameTextView);
         TextView contentTextView = (TextView) mView.findViewById(R.id.contentTextView);
+        ImageView postImage = (ImageView) mView.findViewById(R.id.postImage);
         mPostAuthorId = post.getAuthorId();
         String postAuthorImage = post.getAuthorImageUrl();
+        String postImageBitmap = post.getImage();
+
+        if (postImageBitmap != null) {
+            Bitmap imageBitmap = decodeFromFirebaseBase64(postImageBitmap);
+            postImage.setImageBitmap(imageBitmap);
+            postImage.setVisibility(View.VISIBLE);
+        }
 
         mUserNameTextView.setText(post.getAuthor());
         contentTextView.setText(post.getContent());
@@ -44,6 +57,11 @@ public class FirebasePostViewHolder extends RecyclerView.ViewHolder implements V
                     .into(memberProfileImageView);
         }
         mUserNameTextView.setOnClickListener(this);
+    }
+
+    public static Bitmap decodeFromFirebaseBase64(String image) throws IOException {
+        byte[] decodedByteArray = android.util.Base64.decode(image, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedByteArray, 0, decodedByteArray.length);
     }
 
     @Override
